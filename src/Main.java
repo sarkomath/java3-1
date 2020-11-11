b616362082950fa6381fcb6f674b44a5aa4fcd70
Nikita Novoselov <sarkomath@outlook.com>
2020-11-11 19:48:26 +1000

    fix code

--- src/Main.java
+++ src/Main.java
@@ -1,2 +1,11 @@
-package PACKAGE_NAME;public class Main {
+public class Main {
+    public static void main(String[] args) {
+        int distance = 1;
+        int price = 20;
+        int ticket = 63581;
+        int miles = ticket / price * distance;
+        System.out.println("You have:");
+        System.out.println(miles);
+        System.out.println("miles!");
+    }
 }
